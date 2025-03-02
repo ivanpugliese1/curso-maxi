7bcbe246feb25129c3dd4529832f68f31c30266a
Pugliese <ivanpugliese.f@gmail.com>
2025-03-02 16:33:13 -0300

    primera subida

---
+++ primer-form/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace primer_form
+{
+    public partial class frDatosPersonales : Form
+    {
+        public frDatosPersonales()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAceptar_Click(object sender, EventArgs e)
+        {
+            if (txtApellido.Text == "" || txtNombre.Text == "" || txtEdad.Text == "" || txtDireccion.Text == "")
+            {
+                MessageBox.Show("Faltan datos para completar");
+                txtApellido.BackColor = Color.IndianRed;
+                txtNombre.BackColor = Color.IndianRed;
+                txtEdad.BackColor = Color.IndianRed;
+                txtDireccion.BackColor = Color.IndianRed;
+            }
+            else
+            {
+                listView.Items.Add("Apellido y Nombre: " + txtApellido.Text + " " + txtNombre.Text);
+                listView.Items.Add("Edad: " + txtEdad.Text);
+                listView.Items.Add("Direccion: " + txtDireccion.Text);
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
